a91eaf54d4c8c85fa071390714f02d0e94c226df
Bruno Drugowick <bruno.drugowick@gmail.com>
2020-12-24 01:01:17 -0300

    Adds a new controller with some JS code.

---
+++ src/controllers/clipboard_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "stimulus"
+
+export default class extends Controller {
+
+    static targets = [ "pin" ]
+
+    copy() {
+        console.log("A method to copy stuff to the clipboard. JS needed...")
+        console.log(this.pin)
+
+        // selects whatever is on the element
+        this.pinTarget.select()
+        // copies to the clipboard
+        document.execCommand("copy")
+    }
+
+    get pin() {
+        return this.pinTarget.value;
+    }
+}
