e351b2d2b529d2fea1dcac0665636e3d45978552
Bruno Drugowick <bruno.drugowick@gmail.com>
2020-12-25 22:48:24 -0300

    Now does short polling to update data on the page.

--- src/controllers/content_loader_controller.js
+++ src/controllers/content_loader_controller.js
@@ -1,10 +1,23 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-    static values = { url: String }
+    static values = { 
+        url: String,
+        refreshInterval: Number
+     }
 
     connect() {
         this.load();
+
+        if (this.hasRefreshIntervalValue) {
+            this.startRefreshing()
+        }
+    }
+
+    startRefreshing() {
+        setInterval(() => {
+            this.load()
+        }, this.refreshIntervalValue)
     }
 
     load() {
